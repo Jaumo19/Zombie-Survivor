a45599d929a591449c0f54d7b847bee1392888ee
Jaumo <jaumepicasso13@gmail.com>
2022-05-13 13:31:28 +0200

    Versión 1.1
    Colisión balas arregladas

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-   public GameObject hitEffect;
    void OnCollisionEnter2D(Collision2D collision)
    {
-      GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-      Destroy(effect, 0.4f);
       Destroy(gameObject);
    }
 }
